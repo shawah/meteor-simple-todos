588ba4d7ac16244beb0c8287df4c67b72fc9daf9
S <sarah.kamli.salino@gmail.com>
2017-11-03 00:05:47 +0000

    Database & reactive directory

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,48 @@
 import { Template } from 'meteor/templating';
+ 
+import { ReactiveDict } from 'meteor/reactive-dict';
 
+import { Tasks } from '../api/tasks.js';
+
+import './task.js';
+ 
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+  this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
-	tasks: [
-		{ text: 'This is task 1'},
-		{ text: 'This is task 2'},
-		{ text: 'This is task 3'}
-	],
+  tasks() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+  },
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
+  },
+ 'change .hide-completed input'(event, instance) {
+    instance.state.set('hideCompleted', event.target.checked);
+  },
 });
